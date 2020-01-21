a506693af8a360273e96c1dc0c8c3235f69537e2
Duke <hnguyen1179@berkeley.edu>
2020-01-20 16:20:42 -0800

    Style mern-twitter

--- mern-twitter/app.js
+++ mern-twitter/app.js
@@ -1,15 +1,28 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => res.send("hey!"));
 const db = require("./config/keys").mongoURI;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-mongoose 
-    .connect(db, { useNewURLParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
-const port = process.env.port || 5000;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
+app.get("/", (req, res) => res.send("Hello World!!"));
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
